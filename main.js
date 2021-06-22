704f5c1458ac46a4227b3d97c4ebb1ac5559d31f
AdegokeJaafar <jaafaradegoke@gmail.com>
2021-06-22 22:34:22 +0100

    Add existing file

---
+++ main.js
@@ -0,0 +1,40 @@
+// var time = setInterval(stopWatch, 1000);
+// var count = 0;
+// var hr = 0;
+// var min = 0;
+
+// function stopWatch () {
+//     count = count + 1;
+//     if (count > 59) {
+//         min = min + 1;
+//         count = 0;
+//     } if (min > 59) {
+//         hr += 1;
+//         min = 0;
+//     } if (hr === 6) {
+//         clearInterval(time);
+//     }
+//     console.log(`${hr} hr ${min}m ${count}s`);
+// }
+
+
+let time = setInterval(timer, 1000);
+let count = 0;
+let hr = 2;
+let min = 0;
+console.log(`${hr} hr ${min}m ${count}s`);
+
+function timer () {
+    count = count - 1;
+    if (count < 0) {
+        min = min - 1;
+        count = 59;
+    } if (min < 0) {
+        hr -= 1;
+        min = 59;
+    }
+    // } if (hr === 0 && min === 0 && count === 0) {
+    //     clearInterval(time);
+    // }
+    console.log(`${hr} hr ${min}m ${count}s`);
+}
